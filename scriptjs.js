648f7d2fd1722146c22d9aa81109468da0269e56
Jenni <rybenkova.evgenia@yandex.ru>
2023-06-07 00:52:09 +0300

    Дз по JS

---
+++ scriptjs.js
@@ -0,0 +1,62 @@
+let name10 = prompt ('Как вас зовут?');
+alert (`Привет, &{name10}!`);
+
+const user = {
+	name: 'Jenni',
+	age: 24,
+    isAdmin: 1,
+    city of residence,
+    delete city of residence,
+};
+
+
+let age = Number(prompt ('Сколько Вам лет?')) ;
+alert (age);
+
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = ++num;
+num = --num;
+alert(num);
+
+
+let d = Number ('9');
+let e = Number ('2');
+let result6 = Math.abs(d / e) ;
+alert (`${result6}`);
+
+
+let i = Number ('2');
+let n = Number ('5');
+let result5 = i**5;
+alert (`${result5}`);
+
+
+let b = Number ('10');
+let c = Number ('2');
+let result41 = b + c;
+alert (`${b} + ${c} = ${ result41 }`);
+let result42 = b - c;
+alert (`${b} - ${c} = ${ result42 }`);
+let result43 = b * c;
+alert (`${b} * ${c} = ${ result43 }`);
+let result44 = b / c;
+alert (`${b} / ${c} = ${ result44 }`);
+
+
+let name = "Брендан Эйх или Айк";
+//name = prompt('Имя создателя языка JavaScript')
+alert(name);
+
+
+let year = "2007";
+//year = prompt('Год выпуска первого iPhone')
+alert(year);
+
+
+let a = "10";
+alert(a);
